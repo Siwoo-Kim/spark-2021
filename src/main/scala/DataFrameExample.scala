24c9bb266d0b2668c6c05cf265361ef485d48371
kimsi <Siwoo.Kim@pointclickcare.com>
2021-01-04 11:47:38 -0500

    date & timestamp & null & complex

--- src/main/scala/DataFrameExample.scala
+++ src/main/scala/DataFrameExample.scala
@@ -224,6 +224,151 @@ object DataFrameExample {
         retail.select(filterSeq:_*) //varargs
                 .where($"is_red")
                 .show()
+
+        /**
+         * date and timestamps
+         * 
+         * 1. current time
+         *  - current_date
+         *  - current_timestamp
+         *  
+         * 2. date manipulation
+         *  - date_sub, date_add
+         *  - date_diff, months_between
+         *  
+         * 3. conversion (string -> date or timestamp)
+         *  - to_date, to_timestamp
+         *  
+         * 4. timezone
+         *  spark.sql.session.timeZone
+         *      - session local timezone
+         */
+        section("date & timestamp")
+        spark.conf.set("spark.sql.session.timeZone", "GMT-5")
+        df.select(current_timestamp()).show()
+        
+        spark.conf.set("spark.sql.session.timeZone", "GMT+9")
+        df.select(current_timestamp()).show()
+        
+        val dateDF = spark.range(10).toDF("num")
+                .withColumn("today", current_date())
+                .withColumn("now", current_timestamp())
+        dateDF.createOrReplaceTempView("dates")
+        dateDF.printSchema()
+        
+        dateDF.select(
+            date_sub($"today", 5), 
+            date_add($"today", 5))
+                .show()
+        
+        dateDF.select(
+            $"today",
+            date_sub($"today", 7).as("weekago"))
+                .withColumn("diff", datediff($"today", $"weekago"))
+                .show()
+        
+        dateDF.select(
+            to_date(lit("2020-01-01")).as("start"),
+            to_date(lit("2021-01-01")).as("end")
+        ).withColumn("between", months_between($"end", $"start"))
+                .show()
+        
+        val dateFormat = "yyyy-dd-MM"
+        spark.range(1).select(
+            to_date(lit("2017-12-11"), dateFormat),
+            to_date(lit("2017-20-12"), dateFormat),
+            to_timestamp(lit("2017-12-11"), dateFormat),
+            to_timestamp(lit("2017-20-12"), dateFormat)
+        ).show()
+
+        /**
+         * handling nulls
+         *  - DataFrame.na
+         *  
+         *  fill null column
+         *      1. coalesce
+         *  
+         *  drop null rows
+         *      1. any, all
+         *          any = drop rows in which any value is null
+         *          all = drop rows in which all values are null
+         *          
+         *          DateFrame.na.drop("any")
+         *  
+         *  replace the value
+         *      replace all values in a column according to their value
+         */
+        section("nulls (na)")
+        spark.range(2)
+                .select(coalesce(lit(null), lit("a"), lit("b")))
+                .show()
+
+        spark.range(1).toDF("num")
+                .select(lit(null), lit("a"))
+                .na
+                .drop("any")
+                .show()
+        
+        spark.range(1).toDF("num")
+                .select(lit(null), lit("a"))
+                .na
+                .drop("all")
+                .show()
+        
+        spark.range(10).toDF("num")
+                .na.replace("num", Map(1 -> (1 << 3)))
+                .show()
+
+        /**
+         * complex types
+         *  1. structs
+         *      - DataFrame
+         *      - "." - accessing column
+         *      - "*" - all columns to top level.
+         *  2. arrays.
+         *      - "split" - creating array from string
+         *      - "[]" - accessing a column by index
+         *      - size - length of the array
+         *      - array_contains
+         *      
+         *      explode *
+         *          - 배열인 컬럼 하나를 지정하고 explode 을 수행하면
+         *          각 배열의 원소는 하나의 행 되고 나머지 값은 기존 값을 복제.
+         *          - 주의할 점은 기존 df 의 행의 길이는 변경된다는 것.
+         *  3. map
+         *      - map()
+         *      - "['키']" - accessing a column be key
+         *      - explode - 배열과 같은 기능.
+         *      
+         */
+        section("complex")
+        retail.select(
+            struct($"description", $"invoiceno").as("complex"), 
+            $"*"
+        ).select(
+            $"complex",
+            $"complex.description",
+            $"complex.invoiceno",
+            $"complex.*"
+        ).show(5)
+        
+        retail.select(split($"description", " ").as("array"))
+                .select(
+                    expr("array[0]"), 
+                    expr("array[1]"), 
+                    $"array")
+                .withColumn("length", size($"array"))
+                .withColumn("hasRed", array_contains($"array", "RED"))
+                .show(10)
+        
+        retail.withColumn("array", split($"description", " "))
+                .withColumn("explode", explode($"array"))
+                .show(5)
+        
+        retail.select(map($"description", $"invoiceno").as("map"))
+                .select(expr("map['WHITE METAL LANTERN']"), $"*")
+                .show(5)
+        
         /**
          * distinct rows
          */
@@ -269,5 +414,6 @@ object DataFrameExample {
                 .where($"destination" === "Korea")
                 .coalesce(1)
                 .show()
+        
     }
 }
